5fc6a39ba5dc615ba5ad0dd1bf4e24e494b5110f
Echobot <echobotcsl@gmail.com>
2023-11-07 16:57:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# App de login
+
+## Sobre o projeto
+
+**Aplicação de cadastro e login, salva dados e funciona no login.**
+
+### Como instalar:
+**Baixe o apk no seu smartphone, cerifique-se de que seu smartphone está configurado para aceitar apps de terceiros, assim não terá conflitos.**
+
+### Pré-Requisitos:
+**Android 24 em diante.**
+<br>
+
+### APK:
+<a href=./appLogin.apk>Download APK </a>
+
+### Layout mobile:
+<div>
+<img width=200px src="https://github.com/Arthur-Cardoso-de-Jesus/AppLogin/assets/83030989/673a1055-494d-4e00-b4f4-9efcbffb823e">
+  <img width=200px src="https://github.com/Arthur-Cardoso-de-Jesus/AppLogin/assets/83030989/fbef15e0-b986-48a6-9ec0-5efe8bef433c">
+  <img width=200px src="https://github.com/Arthur-Cardoso-de-Jesus/AppLogin/assets/83030989/47b0f5e0-1de4-4190-8172-419c4a2231de">
+</div>
+
+
+<br><br>
+<hr>
+
+
+## Tecnologias utilizadas:
+
+### BackEnd:
+**Java.**
+
+### FrontEnd:
+**XML.**
+
+### IDE
+**Android studio**
+
+<br></br>
+<hr>
+
+### Autor:
+<a href="https://github.com/Arthur-Cardoso-de-Jesus">  Arthur Cardoso de Jesus  </a>
---
+++ app/src/main/java/br/ulbra/applogin/LoginActivity.java
@@ -0,0 +1,53 @@
+package br.ulbra.applogin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText edLogin, edPass;
+    Button btnLogin;
+
+    DBHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstancesStace){
+        super.onCreate (savedInstancesStace);
+        setContentView(R.layout.activity_login);
+
+        db = new DBHelper(this);
+        edLogin = (EditText) findViewById(R.id.txtLogin);
+        edPass = (EditText) findViewById(R.id.txtSenha);
+        btnLogin = (Button) findViewById(R.id.btnLogar);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edLogin.getText().toString();
+                String password = edPass.getText().toString();
+                if(username.equals("")){
+                    Toast.makeText(LoginActivity.this ,"Usuário não inserido, tente novamente",
+                            Toast.LENGTH_SHORT).show();
+                } else if (password.equals("")) {
+                    Toast.makeText(LoginActivity.this, "Senha não inserida, tente novamente",
+                            Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login: "+username+" pass: "+password, Toast.LENGTH_SHORT).show();
+                    String res = db.validarLogin(username,password);
+
+                    if(res.equals("OK")){
+                        Toast.makeText(LoginActivity.this,"Login OK !!",Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(LoginActivity.this,"Login ou Senha erra(s)!!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
